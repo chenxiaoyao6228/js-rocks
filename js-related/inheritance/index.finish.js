59c7a3ce13b6bda8be659568523b1a73b97a7cf8
chenxiaoyao <chenxiaoyao6228@163.com>
2021-01-05 19:49:50 +0800

    feat(js): add class implementation

--- js-related/inheritance/index.finish.js
+++ js-related/inheritance/index.finish.js
@@ -12,4 +12,48 @@ function myNew(Constructor) {
   }
 }
 
-export { myNew }
+// 定义类, new的时候调用construct
+// extend方法实现继承, 继承要调用super方法,要通过instanceOf检测
+// 静态方法实现
+function Class(classDefinition) {
+  if (!classDefinition) throw 'should provide class definition'
+
+  // 返回目标类的构造函数
+  function getClassBase() {
+    return function Base() {
+      this.construct.apply(this, arguments)
+    }
+  }
+  var Base = getClassBase()
+
+  // 为目标类添加原型成员
+  function createClassDefinition(classDefinition) {
+    var parent = this.prototype['parent'] || (this.prototype['parent'] = {})
+    for (var prop in classDefinition) {
+      if (prop === 'statics') {
+        for (var sprop in classDefinition.statics) {
+          this[sprop] = classDefinition.statics[sprop]
+        }
+      } else {
+        if (typeof classDefinition[prop] === 'function') {
+          var parentMethod = this.prototype[prop]
+          parent[prop] = parentMethod
+        }
+        this.prototype[prop] = classDefinition[prop]
+      }
+    }
+  }
+  createClassDefinition.call(Base, classDefinition)
+
+  Base.extend = function(classDefinition) {
+    var ChildClass = getClassBase()
+    ChildClass.prototype = new this() // 父类的实例作为子类的prototype
+    createClassDefinition.call(ChildClass, classDefinition)
+    ChildClass.extend = this.extend
+    return ChildClass
+  }
+
+  return Base
+}
+
+export { myNew, Class }
