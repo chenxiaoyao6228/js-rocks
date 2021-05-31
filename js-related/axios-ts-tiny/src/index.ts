2bf9cf44915ebf6e44be9f7a170f1b72d15751e9
chenxiaoyao <chenxiaoyao6228@163.com>
2021-05-31 18:51:33 +0800

    fix: fix header transform

--- js-related/axios-ts-tiny/src/index.ts
+++ js-related/axios-ts-tiny/src/index.ts
@@ -1,6 +1,7 @@
 import { AxiosRequestConfig, AxiosPromise } from './types'
 import { bulidURL } from '../src/helpers/url'
 import { transformRequest } from '../src/helpers/data'
+import { processHeader } from '../src/helpers/headers'
 import xhr from './xhr'
 
 function axios(config: AxiosRequestConfig): AxiosPromise {
@@ -10,12 +11,18 @@ function axios(config: AxiosRequestConfig): AxiosPromise {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
+  config.headers = transformHeaders(config)
   config.data = transformRequestData(config)
 }
 
 function transformUrl(config: AxiosRequestConfig): string {
   const { url, params } = config
   return bulidURL(url, params)
+}
+
+function transformHeaders(config: AxiosRequestConfig) {
+  const { headers = {}, data } = config
+  return processHeader(headers, data)
 }
 
 function transformRequestData(config: AxiosRequestConfig): any {
