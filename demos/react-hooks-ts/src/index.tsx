bed40c1b66e1195173b93bb315a077fa4cfe3ee4
chenxiaoyao <chenxiaoyao6228@163.com>
2021-10-26 13:06:29 +0800

    feat: update

---
+++ demos/react-hooks-ts/src/index.tsx
@@ -0,0 +1,6 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+
+export { default as Button } from './components/Button'
+export { default as Icon } from './components/Icon'
--- demos/react/src/LazyLoad/index.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from "react";
-import { Modal, Button } from "antd";
-import "./index.css";
-
-function Demo() {
-  return <div>xx</div>;
-}
-
-export default Demo;
