a79d2fe96bdef1423e71a66bcf6b20f6c8d0d3a0
chenxiaoyao <chenxiaoyao6228@163.com>
2022-01-10 17:13:58 +0800

    docs: update

---
+++ js-related/typescript-exercises/index.js
@@ -0,0 +1,11 @@
+function firstElement(arr) {
+  return arr[0];
+}
+function map(arr, fun) {
+  return arr.map(fun);
+}
+function Longest(a, b) {
+  return a.length > b.length ? a : b;
+}
+
+console.log(Longest("1111", "222"));
