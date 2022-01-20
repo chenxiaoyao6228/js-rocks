721d18840cd3b3e5e6cb76ba68f58d2e518d9ae5
chenxiaoyao <chenxiaoyao6228@163.com>
2022-01-20 11:22:19 +0800

    feat(ts-challenges): chainable options

---
+++ js-related/ts-challenges/12-medium-chainable-options/test-cast.ts
@@ -0,0 +1,19 @@
+import { Alike, Expect } from "@type-challenges/utils";
+
+declare const a: Chainable;
+
+const result = a
+  .option("foo", 123)
+  .option("bar", { value: "Hello World" })
+  .option("name", "type-challenges")
+  .get();
+
+type cases = [Expect<Alike<typeof result, Expected>>];
+
+type Expected = {
+  foo: number;
+  bar: {
+    value: string;
+  };
+  name: string;
+};
