93a3adedec33190f7815d3b072761aa3530dda74
chenxiaoyao <chenxiaoyao6228@163.com>
2022-01-26 19:50:18 +0800

    feat(ts-challenges):  Permutation

---
+++ js-related/ts-challenges/296-medium-permutation/test-case.ts
@@ -0,0 +1,29 @@
+import { Equal, Expect } from "@type-challenges/utils";
+type perm = Permutation<"A" | "B" | "C">;
+
+type cases = [
+  Expect<Equal<Permutation<"A">, ["A"]>>,
+  Expect<
+    Equal<
+      Permutation<"A" | "B" | "C">,
+      | ["A", "B", "C"]
+      | ["A", "C", "B"]
+      | ["B", "A", "C"]
+      | ["B", "C", "A"]
+      | ["C", "A", "B"]
+      | ["C", "B", "A"]
+    >
+  >,
+  Expect<
+    Equal<
+      Permutation<"B" | "A" | "C">,
+      | ["A", "B", "C"]
+      | ["A", "C", "B"]
+      | ["B", "A", "C"]
+      | ["B", "C", "A"]
+      | ["C", "A", "B"]
+      | ["C", "B", "A"]
+    >
+  >,
+  Expect<Equal<Permutation<never>, []>>
+];
