17bc49c90b94a3a973e96ec507bda4ec63ae5934
chenxiaoyao <chenxiaoyao6228@163.com>
2022-01-28 14:35:17 +0800

    feat(ts-challenges): MapTypes

---
+++ js-related/ts-challenges/5821-medium-maptypes/template.ts
@@ -0,0 +1,14 @@
+interface MapConfig {
+  mapFrom: unknown;
+  mapTo: unknown;
+}
+
+type ComputeType<T, R extends MapConfig> = R extends { mapFrom: T }
+  ? R["mapTo"]
+  : never;
+
+type MapTypes<T, R extends MapConfig> = {
+  [P in keyof T]: [ComputeType<T[P], R>] extends [never]
+    ? T[P]
+    : ComputeType<T[P], R>;
+};
