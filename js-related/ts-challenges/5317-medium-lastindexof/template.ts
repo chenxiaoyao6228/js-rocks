074777cd5bb5776a39b1cdbef23781de5e31d5f8
chenxiaoyao <chenxiaoyao6228@163.com>
2022-01-29 19:00:26 +0800

    feat(ts-challenges): add lastIndexOf

---
+++ js-related/ts-challenges/5317-medium-lastindexof/template.ts
@@ -0,0 +1,18 @@
+type LastIndexOf<T extends unknown[], U> = T extends [...infer Rest, infer Last]
+  ? Last extends U
+    ? Rest["length"]
+    : LastIndexOf<Rest, U>
+  : -1;
+
+// js实现
+const len = (arr) => arr.length;
+const last = (arr) => arr[arr.length - 1];
+function lastIndexOf(arr, item) {
+  return arr.length > 0
+    ? last(arr) === item
+      ? arr.length - 1
+      : lastIndexOf(arr.slice(0, -1), item)
+    : -1;
+}
+const a = lastIndexOf([1, 2, 3, 2, 1], 2);
+console.log(a);
