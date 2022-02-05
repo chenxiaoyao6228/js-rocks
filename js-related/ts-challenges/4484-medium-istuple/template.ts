4acb1e03ad0d8a962b2e64e3f62d0368c9131e7e
chenxiaoyao <chenxiaoyao6228@163.com>
2022-02-06 05:46:41 +0800

    feat(ts-challenges): add medium isNever

--- js-related/ts-challenges/4484-medium-istuple/template.ts
+++ js-related/ts-challenges/4484-medium-istuple/template.ts
@@ -1,7 +1,7 @@
 type IsAny<T> = 1 extends T & 0 ? true : false;
-type IsNever<T> = [T] extends [never] ? true : false;
+type _IsNever<T> = [T] extends [never] ? true : false;
 
-type IsTuple<T> = true extends IsAny<T> | IsNever<T>
+type IsTuple<T> = true extends IsAny<T> | _IsNever<T>
   ? false
   : T extends readonly [infer First, ...infer Other] | readonly []
   ? true
