639200abbee03f20e037fafcf4e171f53a0e3543
chenxiaoyao <chenxiaoyao6228@163.com>
2022-02-11 21:08:10 +0800

    docs(ts-challenges): update

--- js-related/ts-challenges/949-medium-anyof/template.ts
+++ js-related/ts-challenges/949-medium-anyof/template.ts
@@ -1,4 +1,5 @@
 // 注意这里的Record<any, never>不能写成{},原因是{}是任意类型的父类型
+// Record是生成索引类型的, 因此Record<any, any>约束了这里只能传入索引类型
 // 具体见stackoverflow.com/questions/60996831/differences-between-recordstring-any-and
 type FalsyValues = [0, "", false, [], Record<any, never>];
 type AnyOf<T extends unknown[]> = T extends [infer Head, ...infer Tail]
