3e80eb92ece4f5fe059bf1bd4a650e04c0b1772b
chenxiaoyao <chenxiaoyao6228@163.com>
2022-02-12 15:18:11 +0800

    feat(ts-challenges): add split

---
+++ js-related/ts-challenges/2822-hard-split/template.ts
@@ -0,0 +1,10 @@
+type Split<
+  S extends string,
+  SEP extends string
+> = S extends `${infer F}${SEP}${infer R}`
+  ? [F, ...Split<R, SEP>]
+  : SEP extends ""
+  ? []
+  : string extends S
+  ? S[]
+  : [S];
