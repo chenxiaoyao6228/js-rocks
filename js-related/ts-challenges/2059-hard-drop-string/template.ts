5e78d5b7ca0dabec26eac5348d5379ef4dccc352
chenxiaoyao <chenxiaoyao6228@163.com>
2022-02-14 21:06:46 +0800

    feat(ts-challenges): add drop string

---
+++ js-related/ts-challenges/2059-hard-drop-string/template.ts
@@ -0,0 +1,13 @@
+type Contains<S extends string, N extends string> = S extends `${any}${N}${any}`
+  ? true
+  : false;
+
+type DropString<
+  S extends string,
+  R extends string,
+  Res extends string = ""
+> = S extends `${infer H}${infer T}`
+  ? Contains<R, H> extends true
+    ? DropString<T, R, `${Res}`>
+    : DropString<T, R, `${Res}${H}`>
+  : Res;
