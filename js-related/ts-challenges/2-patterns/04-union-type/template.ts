20d97542d7ad1e90aac63459cc813598cd095b28
chenxiaoyao <chenxiaoyao6228@163.com>
2022-03-01 15:05:55 +0800

    docs(ts-challenges): update array length as number calculation and union type operation

--- js-related/ts-challenges/2-patterns/04-union-type/template.ts
+++
@@ -1,1 +0,0 @@
-// type _Includes<T extends unknown[], U> = U extends T[number] ? true : false;
--- js-related/ts-challenges/2-patterns/06-trivial/template.ts
+++ js-related/ts-challenges/2-patterns/06-trivial/template.ts
@@ -1,1 +1,5 @@
+// enumer
+
+// array[number]
 type a = [1, 2, 3][number];
+// type _Includes<T extends unknown[], U> = U extends T[number] ? true : false;
