81168e8a03e4d27cd1ca3ba20e62abab3cd631b0
chenxiaoyao <chenxiaoyao6228@163.com>
2022-05-16 23:16:48 +0800

    feat(vue-tiny): add name slots implementation and add some typing

--- packages/vue-tiny/src/runtime-core/componentSlots.ts
+++ packages/vue-tiny/src/runtime-core/componentSlots.ts
@@ -1,5 +1,20 @@
-import { ChildrenType, ComponentInstance } from '../../typings/index';
+import { ChildrenType, ComponentInstance, ShapeFlags } from '../../typings/index';
 
 export function initSlots (instance: ComponentInstance, children: ChildrenType) {
-  instance.slots = Array.isArray(children) ? children : [children];
+  // slots
+  const { vnode } = instance;
+  if (vnode.shapeFlag & ShapeFlags.SLOT_CHILDREN) {
+    normalizeObjectSlots(children, instance.slots);
+  }
+}
+
+function normalizeObjectSlots (children: any, slots: any) {
+  for (const key in children) {
+    const value = children[key];
+    slots[key] = props => normalizeSlotValue(value(props));
+  }
+}
+
+function normalizeSlotValue (value) {
+  return Array.isArray(value) ? value : [value];
 }
