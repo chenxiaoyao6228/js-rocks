8a177ec49e631f8c9683137109901981fcd1fec9
chenxiaoyao <chenxiaoyao6228@163.com>
2022-05-19 00:06:37 +0800

    feat(vue-tiny): get the lastest parent provides when multi parent provides the same value

--- packages/vue-tiny/src/runtime-core/injectApi.ts
+++ packages/vue-tiny/src/runtime-core/injectApi.ts
@@ -6,14 +6,25 @@ import { ComponentInstance } from '../../typings/index';
 // set
 export function provide (key: string, value: any) {
   const currentInstance = getCurrentInstance() as ComponentInstance;
-  currentInstance.provides[key] = value;
+  if (currentInstance) {
+    let { provides } = currentInstance;
+
+    const parentProvides = currentInstance.parent.provides;
+    //  utilize prototyp chain lookup
+
+    if (provides === currentInstance.parent.provides) {
+      provides = currentInstance.provides = Object.create(parentProvides);
+    }
+
+    provides[key] = value;
+  }
 }
 
 // lookup
-export function inject (key: string) {
+export function inject (key: string, defaultValue: any) {
   const currentInstance = getCurrentInstance() as ComponentInstance;
-
-  const parentProvides = currentInstance.parent.provides;
-
-  return parentProvides[key];
+  if (currentInstance) {
+    const parentProvides = currentInstance.parent.provides;
+    return parentProvides[key] || defaultValue;
+  }
 }
