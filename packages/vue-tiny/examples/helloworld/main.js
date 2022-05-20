6de0d1f09212fe3a1ee86bdcb7ff5db47f64172c
chenxiaoyao <chenxiaoyao6228@163.com>
2022-05-20 23:10:41 +0800

    chore(vue-tiny): hanle lodash import issue

--- packages/vue-tiny/examples/helloworld/main.js
+++ packages/vue-tiny/examples/helloworld/main.js
@@ -1,4 +1,4 @@
-import { createApp } from '../lib/vue-tiny.esm.js';
+import { createApp } from '../../lib/vue-tiny.esm.js';
 import { App } from './App.js';
 
 const rootContainer = document.querySelector('#app');
