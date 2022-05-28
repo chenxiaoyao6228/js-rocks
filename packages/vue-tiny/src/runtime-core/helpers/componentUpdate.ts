e4668a4530d6fa935d04a0da3141a59020b1e73a
chenxiaoyao <chenxiaoyao6228@163.com>
2022-05-28 21:46:06 +0800

    feat(vue-tiny): only update component when props change

---
+++ packages/vue-tiny/src/runtime-core/helpers/componentUpdate.ts
@@ -0,0 +1,12 @@
+import { VNode } from '../../../typings';
+
+export function shouldComponentUpdate (preVNode: VNode, nextVNode: VNode) {
+  const { props: preProps } = preVNode;
+  const { props: nextProps } = nextVNode;
+  for (const key in preProps) {
+    if (preProps[key] !== nextProps[key]) {
+      return true;
+    }
+  }
+  return false;
+}
