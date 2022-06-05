1f2d2186a6592ba8220e875ccf53e8f9a57c73d2
chenxiaoyao <chenxiaoyao6228@163.com>
2022-06-05 22:12:44 +0800

    feat(vue-tiny):  add parse element to compiler

--- packages/vue-tiny/src/compiler-core/tests/parse.test.ts
+++ packages/vue-tiny/src/compiler-core/tests/parse.test.ts
@@ -1,6 +1,5 @@
 import { NodeTypes } from '../src/ast';
 import { baseParse } from '../src/parse';
-
 describe('Parse', () => {
   describe('interpolation', () => {
     test('simple interpolation', () => {
@@ -12,6 +11,17 @@ describe('Parse', () => {
           type: NodeTypes.SIMPLE_EXPRESSION,
           content: 'message',
         },
+      });
+    });
+  });
+
+  describe('element', () => {
+    it('simple element div', () => {
+      const ast = baseParse('<div></div>');
+
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.ELEMENT,
+        tag: 'div',
       });
     });
   });
