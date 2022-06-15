67483e673f8d4f7c3c581a51f278f1c872e0d691
chenxiaoyao <chenxiaoyao6228@163.com>
2022-06-15 20:42:13 +0800

    feat(babel-tiny): can generate variable declaration and binary expression

--- packages/babel-tiny/test/generate.test.js
+++ packages/babel-tiny/test/generate.test.js
@@ -3,10 +3,34 @@ const traverse = require('../src/traverse');
 const generate = require('../src/generate');
 
 describe('generate', () => {
-  test('can generate a number', () => {
-    const code = '666';
+  test('can generate const a = 1', () => {
+    const code = `const a = 1;
+    `;
+    console.log('code', code);
     const ast = parse(code);
     const newCode = generate(ast);
-    expect(code).toEqual(newCode);
+    expect(newCode.trim()).toEqual(code.trim());
+  });
+  test('can generate multiple VariableDeclaration', () => {
+    const code = 'const a = 1,b = 2;';
+    console.log('code', code);
+    const ast = parse(code);
+    const newCode = generate(ast);
+    console.log('newCode', newCode);
+    expect(newCode.trim()).toEqual(code.trim());
+  });
+  test('can generate newline', () => {
+    const code = 'const a = 1;\nconst b = 2;';
+    console.log('code', code);
+    const ast = parse(code);
+    const newCode = generate(ast);
+    console.log('newCode', newCode);
+    expect(newCode.trim()).toEqual(code.trim());
+  });
+  test('can generate binary expressions', () => {
+    const code = 'const a = 1 + 2;';
+    const ast = parse(code);
+    const newCode = generate(ast);
+    expect(newCode.trim()).toEqual(code.trim());
   });
 });
