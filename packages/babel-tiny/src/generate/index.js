6684b88d9b059ccff5a3881da9a862daf5c681d0
chenxiaoyao <chenxiaoyao6228@163.com>
2022-06-15 20:42:13 +0800

    feat(babel-tiny): can generate variable declaration and binary expression

--- packages/babel-tiny/src/generate/index.js
+++ packages/babel-tiny/src/generate/index.js
@@ -1,3 +1,4 @@
+// Acorn Parser will remove  white spaces during parsing
 class Printer {
   constructor (node) {
     this.node = node;
@@ -7,20 +8,60 @@ class Printer {
     this[this.node.type](this.node);
     return this.text;
   }
+  space () {
+    this.text += ' ';
+  }
+  nextLine () {
+    this.text += '\n';
+  }
   Program (node) {
     // console.log('node in program', node);
     node.body.forEach(n => {
-      this.text += this[n.type](n);
+      this[n.type](n);
+      this.nextLine();
     });
-    return this.text;
   }
+
   ExpressionStatement (node) {
     // console.log('node in ExpressionStatement', node);
-    return this[node.expression.type](node.expression);
+    this[node.expression.type](node.expression);
+  }
+  VariableDeclaration (node) {
+    // VariableDeclaration might contains multiple declarations
+    // eg: const a=1, b=2;
+    // the init node might be an Binary Expression
+    this.text += node.kind;
+    this.space();
+    node.declarations.forEach((declaratorNode, index) => {
+      if (index !== 0) {
+        this.text += ',';
+      }
+      // console.log('declaratorNode', declaratorNode);
+      this[declaratorNode.id.type](declaratorNode.id);
+      this.space();
+      this.text += '=';
+      this.space();
+      this[declaratorNode.init.type](declaratorNode.init);
+    });
+    this.text += ';';
+  }
+  VariableDeclarator (node) {
+    // console.log('node in VariableDeclarator', node);
   }
   Literal (node) {
     // console.log('node in Literal', node);
-    return node.value;
+    this.text += node.raw;
+  }
+  Identifier (node) {
+    this.text += node.name;
+  }
+  BinaryExpression (node) {
+    // console.log('node in BinaryExpression', node);
+    this[node.left.type](node.left);
+    this.space();
+    this.text += node.operator;
+    this.space();
+    this[node.right.type](node.right);
   }
 }
 
