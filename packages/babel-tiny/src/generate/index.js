80df6f84cc10181d09270519f3bd6d983f1e80cb
chenxiaoyao <chenxiaoyao6228@163.com>
2022-06-15 22:18:51 +0800

    feat(babel-tiny): add sourcemap

--- packages/babel-tiny/src/generate/index.js
+++ packages/babel-tiny/src/generate/index.js
@@ -1,30 +1,56 @@
+const { SourceMapGenerator } = require('source-map');
 // Acorn Parser will remove  white spaces during parsing
 class Printer {
-  constructor (node) {
-    this.node = node;
+  constructor (source, fileName) {
     this.text = '';
+    this.sourceMapGenerator = new SourceMapGenerator({
+      file: fileName + '.map.json',
+    });
+    this.fileName = fileName;
+    this.sourceMapGenerator.setSourceContent(fileName, source);
+    this.printLine = 1;
+    this.printColumn = 0;
   }
-  print () {
+  addMapping (node) {
+    if (node.loc) {
+      this.sourceMapGenerator.addMapping({
+        generated: {
+          line: this.printLine,
+          column: this.printColumn,
+        },
+        source: this.fileName,
+        original: node.loc && node.loc.start,
+      });
+    }
+  }
+  print (node) {
+    this.node = node;
     this[this.node.type](this.node);
     return this.text;
   }
   space () {
     this.text += ' ';
+    this.printColumn++;
   }
   nextLine () {
     this.text += '\n';
+    this.printLine++;
+    this.printColumn = 0;
   }
   endLine () {
     this.text += ';';
   }
   Program (node) {
+    this.addMapping(node);
     // console.log('node in program', node);
     node.body.forEach(n => {
-      this[n.type](n);
+      this[n.type](n) + ';';
+      this.printColumn++;
       this.nextLine();
     });
   }
   CallExpression (node) {
+    this.addMapping(node);
     this.text += node.callee.name;
     this.text += '(';
     node.arguments.forEach((param, index) => {
@@ -38,10 +64,12 @@ class Printer {
   }
 
   ExpressionStatement (node) {
+    this.addMapping(node);
     // console.log('node in ExpressionStatement', node);
     this[node.expression.type](node.expression);
   }
   VariableDeclaration (node) {
+    this.addMapping(node);
     // VariableDeclaration might contains multiple declarations
     // eg: const a=1, b=2;
     // the init node might be an Binary Expression
@@ -50,6 +78,7 @@ class Printer {
     node.declarations.forEach((declaratorNode, index) => {
       if (index !== 0) {
         this.text += ',';
+        this.printColumn++;
       }
       // console.log('declaratorNode', declaratorNode);
       this[declaratorNode.id.type](declaratorNode.id);
@@ -57,20 +86,25 @@ class Printer {
       this.text += '=';
       this.space();
       this[declaratorNode.init.type](declaratorNode.init);
+      this.printColumn++;
     });
     this.endLine();
   }
   VariableDeclarator (node) {
+    this.addMapping(node);
     // console.log('node in VariableDeclarator', node);
   }
   Literal (node) {
+    this.addMapping(node);
     // console.log('node in Literal', node);
     this.text += node.raw;
   }
   Identifier (node) {
+    this.addMapping(node);
     this.text += node.name;
   }
   BinaryExpression (node) {
+    this.addMapping(node);
     // console.log('node in BinaryExpression', node);
     this[node.left.type](node.left);
     this.space();
@@ -79,7 +113,9 @@ class Printer {
     this[node.right.type](node.right);
   }
   FunctionDeclaration (node) {
-    this.text += 'function ';
+    this.addMapping(node);
+    this.text += 'function';
+    this.space();
     // function name
     this[node.id.type](node.id);
 
@@ -102,15 +138,17 @@ class Printer {
     this.text += '};';
   }
   FunctionExpression (node) {
+    this.addMapping(node);
     // console.log('node in  FunctionExpression', node);
   }
   BlockStatement (node) {
+    this.addMapping(node);
     node.body.forEach(n => {
-      console.log('n.type', n.type);
       this[n.type](n);
     });
   }
   ReturnStatement (node) {
+    this.addMapping(node);
     this.text += 'return';
     this.space();
     this[node.argument.type](node.argument);
@@ -118,6 +156,7 @@ class Printer {
   }
   EmptyStatement (node) {}
   ForStatement (node) {
+    this.addMapping(node);
     this.text += 'for(';
     this[node.init.type](node.init);
     // this.text += ';';
@@ -131,6 +170,7 @@ class Printer {
     this.text += '}';
   }
   UpdateExpression (node) {
+    this.addMapping(node);
     // console.log('UpdateExpression', node);
     if (node.prefix === true) {
       this.text += node.operator;
@@ -143,9 +183,22 @@ class Printer {
   // FIXME: more statement: forof forin, while, switch, trycatch, debugger, throw, continue ...
 }
 
-function generate (node) {
-  const printer = new Printer(node);
-  return printer.print(node);
+class Generator extends Printer {
+  constructor (source, fileName) {
+    super(source, fileName);
+  }
+
+  generate (node) {
+    this[node.type](node);
+    return {
+      code: this.text,
+      map: this.sourceMapGenerator.toString(),
+    };
+  }
+}
+
+function generate (node, source, fileName) {
+  return new Generator(source, fileName).generate(node);
 }
 
 module.exports = generate;
--- packages/babel-tiny/test/generate.test.js
+++ packages/babel-tiny/test/generate.test.js
@@ -7,25 +7,25 @@ describe('generate', () => {
     const code = `const a = 1;
     `;
     const ast = parse(code);
-    const newCode = generate(ast);
+    const { code: newCode } = generate(ast);
     expect(newCode.trim()).toEqual(code.trim());
   });
   test('can generate multiple VariableDeclaration', () => {
     const code = 'const a = 1,b = 2;';
     const ast = parse(code);
-    const newCode = generate(ast);
+    const { code: newCode } = generate(ast);
     expect(newCode.trim()).toEqual(code.trim());
   });
   test('can generate newline', () => {
     const code = 'const a = 1;\nconst b = 2;';
     const ast = parse(code);
-    const newCode = generate(ast);
+    const { code: newCode } = generate(ast);
     expect(newCode.trim()).toEqual(code.trim());
   });
   test('can generate binary expressions', () => {
     const code = 'const a = 1 + 2;';
     const ast = parse(code);
-    const newCode = generate(ast);
+    const { code: newCode } = generate(ast);
     expect(newCode.trim()).toEqual(code.trim());
   });
   test('can generate function expression', () => {
@@ -33,7 +33,7 @@ describe('generate', () => {
       function add(a, b){\nreturn a + b;\n};
     `;
     const ast = parse(code);
-    const newCode = generate(ast);
+    const { code: newCode } = generate(ast);
     expect(newCode.trim()).toEqual(code.trim());
   });
   test('can generate call expression', () => {
@@ -41,32 +41,32 @@ describe('generate', () => {
       sum(a, b)
     `;
     const ast = parse(code);
-    const newCode = generate(ast);
+    const { code: newCode } = generate(ast);
     expect(newCode.trim()).toEqual(code.trim());
   });
   test('can generate Boolean literal', () => {
     const code = 'const a = true;';
     const ast = parse(code);
-    const newCode = generate(ast);
+    const { code: newCode } = generate(ast);
     expect(newCode.trim()).toEqual(code.trim());
   });
   test('can generate Null literal', () => {
     const code = 'const a = null;';
     const ast = parse(code);
-    const newCode = generate(ast);
+    const { code: newCode } = generate(ast);
     expect(newCode.trim()).toEqual(code.trim());
   });
   test('can generate Regex literal', () => {
     const code = 'const a = /^[a-z]+/;';
     const ast = parse(code);
-    const newCode = generate(ast);
+    const { code: newCode } = generate(ast);
     expect(newCode.trim()).toEqual(code.trim());
   });
   // statement
   test('can generate for statement', () => {
     const code = 'for(var i = 0; i < 10; i++){\n}';
     const ast = parse(code);
-    const newCode = generate(ast);
+    const { code: newCode } = generate(ast);
     expect(newCode.trim()).toEqual(code.trim());
   });
 });
