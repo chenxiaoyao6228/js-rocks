d5e95132dac536818b2f9e63a571a979b8200906
chenxiaoyao <chenxiaoyao6228@163.com>
2022-06-15 22:05:33 +0800

    docs: remove some comment and log

--- packages/babel-tiny/test/generate.test.js
+++ packages/babel-tiny/test/generate.test.js
@@ -6,25 +6,20 @@ describe('generate', () => {
   test('can generate const a = 1', () => {
     const code = `const a = 1;
     `;
-    console.log('code', code);
     const ast = parse(code);
     const newCode = generate(ast);
     expect(newCode.trim()).toEqual(code.trim());
   });
   test('can generate multiple VariableDeclaration', () => {
     const code = 'const a = 1,b = 2;';
-    console.log('code', code);
     const ast = parse(code);
     const newCode = generate(ast);
-    console.log('newCode', newCode);
     expect(newCode.trim()).toEqual(code.trim());
   });
   test('can generate newline', () => {
     const code = 'const a = 1;\nconst b = 2;';
-    console.log('code', code);
     const ast = parse(code);
     const newCode = generate(ast);
-    console.log('newCode', newCode);
     expect(newCode.trim()).toEqual(code.trim());
   });
   test('can generate binary expressions', () => {
@@ -37,53 +32,41 @@ describe('generate', () => {
     const code = `
       function add(a, b){\nreturn a + b;\n};
     `;
-    console.log('code', code);
     const ast = parse(code);
     const newCode = generate(ast);
-    console.log('newCode', newCode);
     expect(newCode.trim()).toEqual(code.trim());
   });
   test('can generate call expression', () => {
     const code = `
       sum(a, b)
     `;
-    console.log('code', code);
     const ast = parse(code);
     const newCode = generate(ast);
-    console.log('newCode', newCode);
     expect(newCode.trim()).toEqual(code.trim());
   });
   test('can generate Boolean literal', () => {
     const code = 'const a = true;';
-    console.log('code', code);
     const ast = parse(code);
     const newCode = generate(ast);
-    console.log('newCode', newCode);
     expect(newCode.trim()).toEqual(code.trim());
   });
   test('can generate Null literal', () => {
     const code = 'const a = null;';
-    console.log('code', code);
     const ast = parse(code);
     const newCode = generate(ast);
-    console.log('newCode', newCode);
     expect(newCode.trim()).toEqual(code.trim());
   });
   test('can generate Regex literal', () => {
     const code = 'const a = /^[a-z]+/;';
-    console.log('code', code);
     const ast = parse(code);
     const newCode = generate(ast);
-    console.log('newCode', newCode);
     expect(newCode.trim()).toEqual(code.trim());
   });
   // statement
   test('can generate for statement', () => {
     const code = 'for(var i = 0; i < 10; i++){\n}';
-    console.log('code', code);
     const ast = parse(code);
     const newCode = generate(ast);
-    console.log('newCode', newCode);
     expect(newCode.trim()).toEqual(code.trim());
   });
 });
