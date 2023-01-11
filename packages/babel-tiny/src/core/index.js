aeca74969052d183cd46e4a478da5e761afd4943
chenxiaoyao <chenxiaoyao6228@163.com>
2023-01-11 08:14:02 +0800

    feat(vue-jsx): udpate learn demo

--- packages/babel-tiny/src/core/index.js
+++ packages/babel-tiny/src/core/index.js
@@ -4,20 +4,30 @@ const generate = require('../generate');
 const template = require('../template');
 
 function transformSync (code, options) {
-  const ast = parser.parse(code, options.parserOpts);
+  const { plugins, parserOpts, fileName } = options;
+  const ast = parser.parse(code, parserOpts);
 
   const pluginApi = {
     template,
   };
   const visitors = {};
-  options.plugins &&
-    options.plugins.forEach(([plugin, options]) => {
+  plugins &&
+    plugins.forEach(([plugin, options]) => {
       const res = plugin(pluginApi, options);
       Object.assign(visitors, res.visitor);
     });
 
+  options.presets &&
+    options.presets.reverse().forEach(([preset, options]) => {
+      const plugins = preset(pluginApi, options);
+      plugins.forEach(([plugin, options]) => {
+        const res = plugin(pluginApi, options);
+        Object.assign(visitors, res.visitor);
+      });
+    });
+
   traverse(ast, visitors);
-  return generate(ast, code, options.fileName);
+  return generate(ast, code, fileName);
 }
 
 module.exports = {
