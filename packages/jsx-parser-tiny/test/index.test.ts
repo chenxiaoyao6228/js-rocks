b80039b3247a6580d3e8e9d109f09c0e5443bca7
chenxiaoyao <chenxiaoyao6228@163.com>
2023-01-15 20:14:23 +0800

    feat(jsx-parser): support attribute parsing

--- packages/jsx-parser-tiny/test/index.test.ts
+++ packages/jsx-parser-tiny/test/index.test.ts
@@ -1,11 +1,34 @@
 import parse from '../src/parser';
 
 describe('parse', () => {
-  test('test', () => {
-    expect(parse('<div></div>')).toEqual({
-      type: 'div',
-      props: {},
-      children: []
+  describe('basic', () => {
+    test('basic', () => {
+      expect(parse('<div></div>')).toEqual({
+        type: 'div',
+        props: {},
+        children: []
+      });
+      expect(parse('hello')).toEqual({
+        type: '#text',
+        nodeValue: 'hello'
+      });
+      expect(parse('<div>hello world</div>')).toEqual({
+        type: 'div',
+        props: {},
+        children: []
+      });
+    });
+  });
+  describe('attr', () => {
+    test('parse <div id="id1" name="name1"></div>', () => {
+      expect(parse('<div id="id1" name="name1"></div>')).toEqual({
+        type: 'div',
+        props: {
+          id: 'id1',
+          name: 'name1'
+        },
+        children: []
+      });
     });
   });
 });
