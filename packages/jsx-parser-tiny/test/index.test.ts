b55816a325a7c395f084ba330951742d1575fa33
chenxiaoyao <chenxiaoyao6228@163.com>
2023-01-17 23:23:46 +0800

    refactor(jsx-parser): change some function name and extract logic

--- packages/jsx-parser-tiny/test/index.test.ts
+++ packages/jsx-parser-tiny/test/index.test.ts
@@ -1,38 +1,87 @@
 import parse from '../src/parser';
 
-describe('parse', () => {
-  describe('basic', () => {
-    test('basic', () => {
-      expect(parse('<div></div>')).toEqual({
-        type: 'div',
-        props: {},
-        children: []
-      });
-      expect(parse('hello')).toEqual({
-        type: '#text',
-        nodeValue: 'hello'
-      });
-      expect(parse('<div>hello world</div>')).toEqual({
-        type: 'div',
-        props: {},
-        children: []
-      });
-      expect(parse('<!--comment content-->')).toEqual({
-        type: '#comment',
-        nodeValue: 'comment content'
-      });
+describe('basic', () => {
+  test('<div></div>', () => {
+    expect(parse('<div></div>')).toEqual({
+      type: 'div',
+      props: {},
+      children: []
     });
   });
-  describe('attr', () => {
-    test('test', () => {
-      expect(parse('<div id="id1" name="name1"></div>')).toEqual({
-        type: 'div',
-        props: {
-          id: 'id1',
-          name: 'name1'
+
+  test('hello', () => {
+    expect(parse('hello')).toEqual({
+      type: '#text',
+      nodeValue: 'hello'
+    });
+  });
+
+  test('<div>hello world</div>', () => {
+    expect(parse('<div>hello world</div>')).toEqual({
+      type: 'div',
+      props: {},
+      children: [{ type: '#text', nodeValue: 'hello world' }]
+    });
+  });
+  test('<!--comment content-->', () => {
+    expect(parse('<!--comment content-->')).toEqual({
+      type: '#comment',
+      nodeValue: 'comment content'
+    });
+  });
+});
+describe('attr', () => {
+  test('<div id="id1" name="name1"></div>', () => {
+    expect(parse('<div id="id1" name="name1"></div>')).toEqual({
+      type: 'div',
+      props: {
+        id: 'id1',
+        name: 'name1'
+      },
+      children: []
+    });
+  });
+});
+
+describe.skip('jsx', () => {
+  test('<div >{111}</div>', () => {
+    expect(parse('<div >{111}</div>')).toEqual({
+      type: 'div',
+      props: {},
+      children: [
+        {
+          type: '#jsx',
+          nodeValue: '111'
+        }
+      ]
+    });
+  });
+  test('<div>xx{111}xx{222}xx</div>', () => {
+    expect(parse('<div>xx{111}yy{222}zz</div>')).toEqual({
+      type: 'div',
+      props: {},
+      children: [
+        {
+          type: '#text',
+          nodeValue: 'xx'
         },
-        children: []
-      });
+        {
+          type: '#jsx',
+          nodeValue: '111'
+        },
+        {
+          type: '#text',
+          nodeValue: 'yy'
+        },
+        {
+          type: '#jsx',
+          nodeValue: '222'
+        },
+        {
+          type: '#text',
+          nodeValue: 'zz'
+        }
+      ]
     });
   });
 });
